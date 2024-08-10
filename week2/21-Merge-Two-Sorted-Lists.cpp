3f691660596bb6172e1937263c8c9fec44dce04a
SriAlanyaReddy <164356102+SriAlanyaReddy@users.noreply.github.com>
2024-08-10 11:03:35 +0530

    [21] [Time Beats: 100.00%] [Memory Beats: 28.76%] - LeetPush

---
+++ week2/21-Merge-Two-Sorted-Lists.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1 == nullptr) return l2;
+        if (l2 == nullptr) return l1;
+        
+        ListNode* dummy = new ListNode(0); // Dummy node to simplify edge cases
+        ListNode* curr = dummy;
+        
+        while (l1 != nullptr && l2 != nullptr) {
+            if (l1->val <= l2->val) {
+                curr->next = l1;
+                l1 = l1->next;
+            } else {
+                curr->next = l2;
+                l2 = l2->next;
+            }
+            curr = curr->next;
+        }
+        
+        // Attach the remaining elements
+        if (l1 != nullptr) {
+            curr->next = l1;
+        } else {
+            curr->next = l2;
+        }
+        
+        return dummy->next; // Return the merged list, starting from the first element after the dummy node
+    }
+};
